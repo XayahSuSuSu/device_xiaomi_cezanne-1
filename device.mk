5d5c1f9edaef2a9c4fbdb98add5e6028be6eeda2
Lokesh Gidra <lokeshgidra@google.com>
2021-09-18 16:55:52 +0800

    cezanne: Use 6GB dalvik-heap config
    Bug: 145823510
    Test: MPTS
    Change-Id: I086d4a5c09634c3c7bd51ec5d08dcaa1551511b4

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default
