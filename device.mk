e63b49af9e02748b78c18444d24278dd7bc23ddf
Hridya Valsaraju <hridya@google.com>
2021-09-18 17:00:10 +0800

    cezanne: Build fastboot daemon for recovery.
     * So we can flash our relevant dynamic partitions.
    
    Change-Id: I372dd9457a824868ef7344dcd861b5b56af408c0

--- device.mk
+++ device.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
     libvulkan \
     vendor.display.config@1.0
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
