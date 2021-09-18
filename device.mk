57f52c838026045b210825d86ebc68b6161b6c9e
LuK1337 <priv.luk@gmail.com>
2021-09-18 17:31:24 +0800

    cezanne: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- device.mk
+++ device.mk
@@ -35,7 +35,11 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor
+    android.hidl.base@1.0.vendor \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Net
 PRODUCT_PACKAGES += \
