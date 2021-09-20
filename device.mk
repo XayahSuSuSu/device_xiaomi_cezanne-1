196022df750162f19c12776a3b15083b646512f2
Kshitij Gupta <kshitijgm@gmail.com>
2021-09-20 21:19:23 +0800

    cezanne: Build libhwbinder/libhidltransport deps
    Change-Id: I044913fd5d432205e69e704befad0660e01a4267

--- device.mk
+++ device.mk
@@ -126,6 +126,11 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor \
+    libunwindstack.vendor \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
