fd90fef4e0788b1c567824edbf1906689437d878
Ethan Chen <intervigil@gmail.com>
2021-09-20 21:19:23 +0800

    cezanne: Build AOSP RCS Packages
    Change-Id: Ia0420967835ba448219305bc66a5afc4428d075b

--- device.mk
+++ device.mk
@@ -276,6 +276,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.mt6885:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6885
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
