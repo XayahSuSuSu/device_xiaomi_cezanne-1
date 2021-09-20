55e998fbb4822bbcb01ec2b8404d7238a74f0b14
Xayah <zds1249475336@gmail.com>
2021-09-20 21:19:23 +0800

    cezanne: Drop lineage trust

--- device.mk
+++ device.mk
@@ -331,10 +331,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Vibrator
 PRODUCT_PACKAGES += \
      android.hardware.vibrator@1.0-impl \
