c92227e008c7959da7c62dd8b1fb7b3fa6c78374
dianlujitao <dianlujitao@lineageos.org>
2021-09-20 14:46:48 +0800

    cezanne: Build dummy android.hidl.[base,manager]@1.0
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- device.mk
+++ device.mk
@@ -103,6 +103,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
