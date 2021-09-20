17918e312fbbe22ac2bf010ac9d1ad3dbb75cc6a
Zinadin Zidan <zidan44@pixelexperience.org>
2021-09-20 21:17:55 +0800

    cezanne: wifi: Build Wi-Fi Hals from MTK opensource
    * As wifi 1.4 hals are giving hotspot errors.
    
    Change-Id: I107f08d4b42b0c281e570f148c964859e0386703

--- device.mk
+++ device.mk
@@ -302,14 +302,5 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# WiFi
-PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wlan/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/wlan/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
-    $(LOCAL_PATH)/configs/wlan/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
 # Inherit vendor
 $(call inherit-product, vendor/xiaomi/cezanne/cezanne-vendor.mk)
