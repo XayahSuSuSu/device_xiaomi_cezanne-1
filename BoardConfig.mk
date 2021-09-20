3a8f7f6abcc3c7ce3ac4b47e2613576e515053a1
Kshitij Gupta <kshitijgm@gmail.com>
2021-09-20 21:18:17 +0800

    cezanne: Import required system|vendor properties
    Change-Id: I6e3b884c7f4c2a15e27162338629e5ac35e81c37

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,6 +136,10 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM_EXT := system/system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6885
