5c447b8162d73eaca658c86bf864d6dbe275d883
Xayah <zds1249475336@gmail.com>
2021-09-20 21:19:23 +0800

    cezanne: Build Gatekeeper

--- device.mk
+++ device.mk
@@ -84,6 +84,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.gatekeeper@1.0-impl
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
