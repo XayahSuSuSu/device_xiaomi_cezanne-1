85026cb84f694ec48317037a463392b196a010a9
Xayah <zds1249475336@gmail.com>
2021-10-30 01:28:43 +0800

    cezanne: Drop MTK repos

--- device.mk
+++ device.mk
@@ -12,13 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# MTK Vendor Repos
-MTK_PLATFORM_FOLDER := mt6885
-$(call inherit-product, vendor/mediatek/ims/mtk-ims.mk)
-$(call inherit-product, vendor/mediatek/ims/mtk-engi.mk)
-$(call inherit-product, vendor/mediatek/ims/mtk-rill.mk)
-$(call inherit-product, device/mediatek/common/target.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
